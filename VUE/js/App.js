ed62c8117a4f50957f7ff2af00d75a04cd94106d
Zac <a19860101@gmail.com>
2022-11-05 20:51:52 +0800

    20221105

--- VUE/js/App.js
+++ VUE/js/App.js
@@ -1,8 +1,12 @@
+import Nav from './components/Nav.js';
 export default {
     data(){
         return {
             title: 'Hello Vue',
             qty: 123,
         }
+    },
+    components:{
+        'app-nav':Nav
     }
 }
